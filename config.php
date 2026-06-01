fd5ffe3e80fcde51b8a79e3113eb8920ca6f335b
Mark Kevin Canonoy <markkevin.canonoy@evsu.edu.ph>
2026-06-01 11:19:54 +0800

    Fix CORS reflection

--- config.php
+++ config.php
@@ -4,7 +4,11 @@
 // Enable CORS for Flutter web / mobile access
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: *");
+if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+    header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+} else {
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
